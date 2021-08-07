ced0c2eb70953e2fce70d4acc0f11f08ba040bb7
ljh97820 <ljh97820@naver.com>
2021-08-07 18:00:09 +0900

    I completed my first toy project, the BMI CALCULATOR period : 21. 8. 6 ~ 21. 8. 7 About 5 hours language : swift feature : without storyboard

--- BMI Calculator/SceneDelegate.swift
+++ BMI Calculator/SceneDelegate.swift
@@ -13,8 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let scene = (scene as? UIWindowScene) else { return }
+        
         self.window = UIWindow(windowScene: scene)
         window?.windowScene = scene
         window?.rootViewController = BMIController()
